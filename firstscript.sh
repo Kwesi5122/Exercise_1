eb7879ac0a73a2e36a2fdf8b53cca6cda2231d0a
Kwesi <nyan5122@outlook.com>
2024-04-06 13:54:26 +0100

    Add firstscript.sh with Bash script

---
+++ firstscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Echo the statement “start of my first script
+echo "start of my first script"
+
+#Create a directory called devops
+mkdir devops
+
+#Change directory into the new folder 
+cd devops
+
+#list the content
+ls
+
+#Create a file called script.sh in the new folder
+touch script.sh
+
+#Echo the statement “end of my first script”
+echo "end of my first script"
